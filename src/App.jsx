acc77ac701f7a50d81b12b6906e85c9394c6bf4c
Rumeysa-koseoglu <koseoglurumeysa0@gmail.com>
2025-05-03 12:36:07 +0300

    feat: add subheading and wrap course cards in a container

--- src/App.jsx
+++ src/App.jsx
@@ -3,17 +3,23 @@ import './App.css'
 import Header from './Header'
 import { courses } from './Data'
 import Course from './Course'
+import './css/Course.css'
 
 function App() {
 
   return (
     <>
     <Header/>
+<div>
+<p className='subheading-all-courses'>All Courses</p>
+</div>
+    <div className='course-main'>
     {
       courses?.map((course) => (
         <Course key={course.id} course={course} /** course={course} is our props  */ /> 
       ))
     }
+    </div>
     </>
   )
 }
